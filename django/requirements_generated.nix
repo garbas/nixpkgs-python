d9fc631e11e7e2f495621876b31bd4e32069c2a4
travis <travis@garbas.si>
2016-08-07 05:25:19 +0000

    Travis update for django [skip ci]

--- django/requirements_generated.nix
+++ django/requirements_generated.nix
@@ -1,7 +1,7 @@
-# generated using pypi2nix tool (version: 1.1.1)
+# generated using pypi2nix tool (version: 1.4.0.dev0)
 #
 # COMMAND:
-#   pypi2nix -r django/requirements.txt
+#   pypi2nix -v -V 3.5 -r requirements.txt
 #
 
 { pkgs, python, commonBuildInputs ? [], commonDoCheck ? false }:
@@ -9,17 +9,17 @@
 self: {
 
   "Django" = python.mkDerivation {
-    name = "Django-1.9.7";
+    name = "Django-1.10";
     src = pkgs.fetchurl {
-      url = "https://pypi.python.org/packages/50/76/aeb1bdde528b23e76df5964003e3e4e734c57c74e7358c3b2224987617dd/Django-1.9.7.tar.gz";
-      md5= "7de9ba83bfe01f4b7d45645c1b259c83";
+      url = "https://pypi.python.org/packages/18/5c/3cd8989b2226c55a1faf66f1a110e76cba6e6ca5d9dd15fb469fb636f378/Django-1.10.tar.gz";
+      sha256= "46b868d68e5fd69dd9e05a0a7900df91786097e30b2aa6f065dd7fa3b22f7005";
     };
     doCheck = commonDoCheck;
     buildInputs = commonBuildInputs;
     propagatedBuildInputs = [ ];
-    meta = {
+    meta = with pkgs.stdenv.lib; {
       homepage = "";
-      license = "BSD";
+      license = licenses.bsdOriginal;
       description = "A high-level Python Web framework that encourages rapid development and clean, pragmatic design.";
     };
   };
