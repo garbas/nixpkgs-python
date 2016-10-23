1284e53e39534ae291f6401d4d427a1882814885
Rok Garbas <rok@garbas.si>
2016-10-23 15:28:39 +0200

    comment out every other collection until we find maintainers

--- default.nix
+++ default.nix
@@ -3,10 +3,12 @@
 
 {
 
-  tools = import ./tools/requirements.nix { inherit pkgs; };
+  # maintainers: garbas
   flask = import ./flask/requirements.nix { inherit pkgs; };
-  pyramid = import ./pyramid/requirements.nix { inherit pkgs; };
-  django = import ./django/requirements.nix { inherit pkgs; };
-  science= import ./science/requirements.nix { inherit pkgs; };
+
+  # XXX: currently looking maintainers
+  #pyramid = import ./pyramid/requirements.nix { inherit pkgs; };
+  #django = import ./django/requirements.nix { inherit pkgs; };
+  #science= import ./science/requirements.nix { inherit pkgs; };
 
 }
