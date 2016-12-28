6eb1da9120154306a9e712a474a50ae37764500c
Rok Garbas <rok@garbas.si>
2016-12-28 09:48:20 +0100

    static now generates json

--- default.nix
+++ default.nix
@@ -1,13 +1,59 @@
 { pkgs ? import <nixpkgs> {}
 }:
 
-{
+let
 
-  django = import ./django/requirements.nix { inherit pkgs; };
-  flask = import ./flask/requirements.nix { inherit pkgs; };
-  homeassistant = import ./homeassistant/requirements.nix { inherit pkgs; };
-  pelican = import ./pelican/requirements.nix { inherit pkgs; };
-  pyramid = import ./pyramid/requirements.nix { inherit pkgs; };
-  science= import ./science/requirements.nix { inherit pkgs; };
+  createSet = meta:
+    (import (./. + "/${meta.name}/requirements.nix") { inherit pkgs; }) // { __meta__ = meta; };
 
-}
+  createSets = sets:
+    builtins.listToAttrs
+      (map (x: { inherit (x) name;
+                 value = createSet x;
+               }
+           ) sets);
+
+  maintainers = {
+    unknown =
+      { name = "??? (Looking for maintainer)";
+        url = "https://github.com/garbas/nixpkgs-python/issues/new";
+      };
+    garbas =
+      { name = "Rok Garbas";
+        url = "https://twitter.com/garbas";
+      };
+  };
+
+in createSets [
+
+  { name = "django";
+    maintainers = [ maintainers.unknown];
+    description = "Django framework and its extensions.";
+  }
+
+  { name = "flask";
+    maintainers = [ maintainers.garbas ];
+    description = "Flask framework and its extensions.";
+  }
+
+  { name = "homeassistant";
+    maintainers = [ maintainers.unknown ];
+    description = "Homeassistant framework and its components.";
+  }
+
+  { name = "pelican";
+    maintainers = [ maintainers.garbas ];
+    description = "Pelican static blog generator and its extensions.";
+  }
+
+  { name = "pyramid";
+    maintainers = [ maintainers.unknown ];
+    description = "Pyramid framework and its extensions.";
+  }
+
+  #{ name = "science";
+  #  maintainers = [ maintainers.unknown ];
+  #  description = "Science related python packages.";
+  #}
+
+]
--- static.nix
+++ static.nix
@@ -5,18 +5,34 @@ let
 
   nixpkgs_python = import ./default.nix { inherit pkgs; };
 
-  getMeta = set:
-      { name: name };
+  pkg_meta = set: pkg:
+    let
+      drv = builtins.parseDrvName (pkgs.lib.removePrefix "${set.__old.python.libPrefix}-" pkg.name);
+    in
+      { inherit (drv) name version;
+        inherit (pkgs.meta) maintainers license homepage description;
+      };
 
-  getMetaPkgs = set:
+  self_meta =
+    map
+      (name:
+        let
+          set = builtins.getAttr name nixpkgs_python;
+        in
+          { inherit (set.__meta__) name maintainers description;
+            packages = map (x: pkg_meta set x) (builtins.attrValues set.packages);
+          }
+      )
+      (builtins.attrNames nixpkgs_python);
+
+  self_meta_file = pkgs.writeText "nixpkgs-python-meta" (builtins.toJSON self_meta);
+
   self = pkgs.stdenv.mkDerivation {
     name = "pypi-nixpkgs-python";
     buildCommand = ''
       mkdir $out
+      cp ${self_meta_file} $out/all.json
     '';
-    meta =
-      map (x: getMeta (builtins.getAttr x nixpkgs_python))
-          (builtins.attrNames nixpkgs_python);
   };
 
 in self
