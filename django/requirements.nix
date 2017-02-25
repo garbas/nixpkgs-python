c3024b748def7faf0c928c6768143bb68cf190cd
travis <travis@garbas.si>
2017-02-25 14:55:24 +0000

    Travis update for django

--- django/requirements.nix
+++ django/requirements.nix
@@ -63,8 +63,22 @@ let
 
   python = withPackages {};
 
-  generated = import ./requirements_generated.nix
-      { inherit pkgs python commonBuildInputs commonDoCheck; };
+  generated = self: {
+
+    "Django" = python.mkDerivation {
+      name = "Django-1.10.5";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/c3/c2/6096bf5d0caa4e3d5b985ac72e3a0c795e37fa7407d6c85460b2a105b467/Django-1.10.5.tar.gz"; sha256 = "0db89374b691b9c8b057632a6cd64b18d08db2f4d63b4d4af6024267ab965f8b"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "";
+        license = licenses.bsdOriginal;
+        description = "A high-level Python Web framework that encourages rapid development and clean, pragmatic design.";
+      };
+    };
+
+  };
   overrides = import ./requirements_override.nix { inherit pkgs python; };
 
 in python.withPackages (fix' (extends overrides generated))
