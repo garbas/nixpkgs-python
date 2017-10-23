a91b9ad062c3f5ddeec586e3e060544ef86fc61b
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2017-10-23 17:46:32 +0200

    Make static.nix build again

--- static.nix
+++ static.nix
@@ -5,6 +5,8 @@ let
 
   nixpkgs_python = import ./default.nix { inherit pkgs; };
 
+  excludePackages = [ "science" ];
+
   pkg_meta = set: pkg:
     let
       drv = builtins.parseDrvName (pkgs.lib.removePrefix "${set.__old.python.libPrefix}-" pkg.name);
@@ -23,7 +25,10 @@ let
             packages = map (x: pkg_meta set x) (builtins.attrValues set.packages);
           }
       )
-      (builtins.attrNames nixpkgs_python);
+      ( builtins.filter
+        (name: ! (builtins.elem name excludePackages))
+        (builtins.attrNames nixpkgs_python)
+      );
 
   self_meta_file = pkgs.writeText "nixpkgs-python-meta" (builtins.toJSON self_meta);
 
