468146eebcc23531ca2352d2da6941ebcc1f8dab
Rok Garbas <rok@garbas.si>
2018-01-15 23:16:24 +0100

    pytest: parse-type has some problems detecting python version

--- pytest/overrides.nix
+++ pytest/overrides.nix
@@ -9,4 +9,13 @@ self: super: {
         old.propagatedNativeBuildInputs;
   });
 
+  "parse-type" = python.overrideDerivation super."parse-type" (old: {
+    patchPhase = ''
+      sed -i \
+        -e "s|\"enum34; python_version < '3.4'\",||" \
+        -e "s|\"ordereddict; python_version < '2.7'\",||" \
+          setup.py
+    '';
+  });
+
 }
