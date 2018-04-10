3e77719bd801621d980cfcc4d5ec0a0c9e778a29
travis <travis@garbas.si>
2018-04-10 18:34:59 +0000

    Travis update for ckan [ci skip]

--- ckan/requirements.nix
+++ ckan/requirements.nix
@@ -474,7 +474,7 @@ let
 
     "ckan" = python.mkDerivation {
       name = "ckan-2.9.0a0";
-      src = pkgs.fetchurl { url = "https://github.com/ckan/ckan/archive/master.zip"; sha256 = "2ae3ea4916b3258efd51951e65ce8af21d8cdc252544fb0e76bc21e00ac5f8e9"; };
+      src = pkgs.fetchurl { url = "https://github.com/ckan/ckan/archive/master.zip"; sha256 = "0efd141990aed589f65e5d839c5a922e83dc5ef8f70b76664e8092cbd7d17e97"; };
       doCheck = commonDoCheck;
       buildInputs = commonBuildInputs;
       propagatedBuildInputs = [ ];
